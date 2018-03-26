87c311e2578caba416c2dc17763c4352944439fd
RafalJDev <jaszczynski.rafal@gmail.com>
2018-03-26 08:00:46 +0200

    Trying commit 2

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import test.TestClass;
+
+/**
+ * Created by Jaszczynski.Rafal on 27.02.2018.
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("say hello from here");
+
+    TestClass testClass = new TestClass();
+    testClass.saySth();
+  }
+
+}
---
+++ src/main/java/test/TestClass.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * Created by Jaszczynski.Rafal on 26.03.2018.
+ */
+public class TestClass {
+
+  public void saySth() {
+    System.out.println("sthh");
+  }
+}
